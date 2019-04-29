a0d97626950f5a9ac8e821a93d3e41d53bdd7de5
todoroff <stoyansky@gmail.com>
2019-04-29 06:03:35 +0100

    operations with integers, handling big numbers

--- js/app.js
+++ js/app.js
@@ -1,8 +1,75 @@
 import * as lib from "./lib.js";
-
-const operate = (cb, ...theArgs) => {
-    return cb(...theArgs);
+import Big from "./big.js";
+const operations = {
+    '+': lib.add,
+    '-': lib.sutbract,
+    '×': lib.multiply,
+    '÷': lib.divide
 }
 
+const operate = (match, a, operation, b) => {
+    return Big(operations[operation](Number(a), Number(b))).toFixed();
+}
 
-console.log(lib.sutbract(lib.add(5, lib.divide(2,2), lib.multiply(4,3)), 10));
+let expression = "0";
+const buildExpression = (symbol) => {
+    if (expression === "0" && symbol !== "0") {
+        expression = symbol;
+
+    } else {
+        expression += symbol;
+    }
+    display.innerText = expression;
+
+    console.log(expression);
+}
+let count = 0;
+//let bigNumbers = [];
+const evaluate = (expr) => {
+    let result = expr;
+    //const bigNum = /([\+\-]+(\d+)(\.)(\d+e\+\d+)*)/g;
+    const bigNum = /(\d+\.)*\d*e\+\d+/g;
+
+    console.log(count++);
+    if (bigNum.test(result)) {
+        let normalize = Big(result.match(bigNum)[0]).toFixed();
+        result = result.replace(bigNum, normalize);
+        return evaluate(result);
+    }
+    const zeroDivision = /\÷0/g;
+    if (zeroDivision.test(result))
+        return '(Err: zero divison)';
+    if (! /[\÷\×\+\-]/.test(result)) {
+        if (result.length > 17)
+            return Big(result).toExponential();
+        return result;
+    }
+    else if (/[\÷\×]/.test(result)) {
+        result = result.replace(/(\d+)([\÷\×])(\d+)/g, operate);
+        return evaluate(result);
+    }
+    else if (/[\+\-]/.test(result)) {
+        result = result.replace(/(\d+)([\+\-])(\d+)/g, operate);
+        return evaluate(result);
+    }
+    return result;
+}
+console.log(evaluate("5÷2+3"));
+
+const display = document.querySelector("#display");
+display.innerText = "0";
+
+const appendableBtns = document.querySelectorAll("button:not(.nonexpr)");
+appendableBtns.forEach(btn => {
+    btn.addEventListener("click", (el) => {
+        buildExpression(el.target.innerText);
+    })
+})
+const equalsBtn = document.querySelector("#equals");
+equalsBtn.addEventListener("click", () => {
+    expression = evaluate(display.innerText);
+    display.innerText = expression;
+});
+console.log(equalsBtn);
+
+
--- js/lib.js
+++ js/lib.js
@@ -1,7 +1,7 @@
-export const add = (...theArgs) => {
-    return theArgs.reduce((runningTotal, number) => runningTotal + number);
+export const sum = (...theArgs) => {
+    return theArgs.reduce((runningTotal, number) => Number(runningTotal) + Number(number));
 }
-
+/*
 export const sutbract = (...theArgs) => {
     return theArgs.reduce((runningTotal, number) => runningTotal - number);
 }
@@ -12,7 +12,24 @@ export const multiply = (...theArgs) => {
 
 export const divide = (...theArgs) => {
     return theArgs.reduce((cumulativeProduct, number) => cumulativeProduct / number);
+}*/
+export const add = (a, b) => {
+    return a + b;
 }
 
+export const sutbract = (a, b) => {
+    return a - b;
+}
 
+export const multiply = (a, b) => {
+    return a * b;
+}
+
+export const divide = (a, b) => {
+    return a / b;
+}
+
+export const negate = (a) => {
+    return -a;
+}
 
