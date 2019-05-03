48b946970e8362616a07588a8355f1a30a9d6846
todoroff <stoyansky@gmail.com>
2019-05-03 06:24:25 +0100

    remove commented code

--- js/lib.js
+++ js/lib.js
@@ -1,18 +1,3 @@
-/*export const sum = (...theArgs) => {
-    return theArgs.reduce((runningTotal, number) => Number(runningTotal) + Number(number));
-}
-
-export const sutbract = (...theArgs) => {
-    return theArgs.reduce((runningTotal, number) => runningTotal - number);
-}
-
-export const multiply = (...theArgs) => {
-    return theArgs.reduce((cumulativeProduct, number) => cumulativeProduct * number);
-}
-
-export const divide = (...theArgs) => {
-    return theArgs.reduce((cumulativeProduct, number) => cumulativeProduct / number);
-}*/
 export const add = (a, b) => {
     return a + b;
 }
